17fddc45d89dae5f76cffbac7a2bf856795f880d
amitmiz-175 <57402092+amitmiz-175@users.noreply.github.com>
2019-12-17 18:50:49 +0200

    hash.c update

--- hash.c
+++ hash.c
@@ -6,6 +6,7 @@
 
 //--------definition of struct hash table--------//
 typedef struct _Hash {
+    int hashTable_size;
     pElement* table_head;
     HashFunc pfunc_search;
     PrintFunc pfunc_print;
@@ -14,15 +15,16 @@ typedef struct _Hash {
     DestroyFunc pfunc_destroy;
 } hashTable;
 
+
 //------definition of struct node element--------//
-typedef struct _nodeElement {
-
-} node_element;
-
+// definition of node in linked list
 typedef struct _pNode_Element *pNode_Element;
 
-//------definition of struct node element--------//
-typedef struct _dElement d_element; // not sure if this is needed...
+typedef struct _nodeElement {
+    pKey pElement_key; // not sure if needed...
+    pElement pData;
+    pNode_Element pNext_node;
+} node_element;
 
 //-------------------------------------helping functions-----------------------------------//
 void malloc_error()
@@ -47,13 +49,13 @@ pHash HashCreate (int size, HashFunc pfunc_search, PrintFunc pfunc_print, Compar
                   GetKeyFunc pfunc_getKey, DestroyFunc pfunc_destroy)
 {
     pHash p_HashTable = NULL;
+    pNode_Element pNodeElement = NULL;
     p_HashTable = (pHash)malloc(sizeof(hashTable)); //is this ok? i'm not sure..
-
-    if (p_HashTable == NULL)
+    pNodeElement = (pNode_Element)malloc(size*(sizeof(node_element)));
+    if (p_HashTable == NULL || pNodeElement == NULL)
     {
         malloc_error();
     }
-
     return p_HashTable;
 }
 
@@ -66,7 +68,18 @@ Return value: Result - FAIL if couldn't add to the table, else SUCCESS
 **********************************************************************************/
 Result HashAdd (pHash p_HashTable, pElement p_element)
 {
+    pNode_Element pNode_newElem = NULL;
+        //create node for new element:
+    pNode_newElem = (pNode_Element)malloc(sizeof(node_element));
+    if (pNode_newElem == NULL)
+    {
+        malloc_error();
+    }
+        //extract element's key:
+    pKey newElem_key = p_HashTable->pfunc_getKey(p_element);
+        //assign element's info to node:
 
+    int place_in_hashTable = p_HashTable->pfunc_search(newElem_key, p_HashTable->hashTable_size);
 }
 
 /*********************************************************************************
@@ -76,7 +89,12 @@ Parameters: p_HashTable - a pointer to the hash table
             p_key - a pointer to the key of an element
 Return value: pElement - a pointer to the element with the key given
 **********************************************************************************/
-pElement HashFind (pHash p_HashTable, pKey p_key);
+pElement HashFind (pHash p_HashTable, pKey p_key)
+{
+    int place_in_hashTable = p_HashTable->pfunc_search(p_key, p_HashTable->hashTable_size);
+    pElement* current = p_HashTable->table_head;
+    //איך מקדמים את current כמו איברים במערך? ואיך משווים את האינדקס?
+}
 
 /*********************************************************************************
 Function name: HashRemove
@@ -85,7 +103,10 @@ Parameters: p_HashTable - a pointer to the hash table
             p_key - a pointer to the key of an element
 Return value: Result - FAIL if couldn't remove, else SUCCESS
 **********************************************************************************/
-Result HashRemove (pHash p_HashTable, pKey p_key);
+Result HashRemove (pHash p_HashTable, pKey p_key)
+{
+    pNode_Element p_elem_found =
+}
 
 /*********************************************************************************
 Function name: HashPrint
